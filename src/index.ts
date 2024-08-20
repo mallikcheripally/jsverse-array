e8601e206c4317cac731b1202d084bc5b6ec67c2
Mallik Cheripally <mallikcheripally@gmail.com>
2024-08-20 17:22:58 +0530

    feat: update export methods in index.ts
    Added new utility methods including average, median, mode, shuffle, slidingWindow, sum, and unique to the main export file. This change enhances the functionality and accessibility of these methods.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export { pluck } from '@/methods/pluck';
-
+export { default as average } from "@/methods/average";
+export { default as median } from "@/methods/median";
+export { default as mode } from "@/methods/mode";
+export { default as pluck } from "@/methods/pluck";
+export { default as shuffle } from "@/methods/shuffle";
+export { default as slidingWindow } from "@/methods/slidingWindow";
+export { default as sum } from "@/methods/sum";
+export { default as unique } from "@/methods/unique";
